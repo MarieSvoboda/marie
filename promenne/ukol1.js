ecf85158b2ac7f474ab263c2f0c49b0be601adbc
MarieSvoboda <mfial@seznam.cz>
2022-02-22 18:07:40 +0100

    1 file modified

--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -1,3 +1,14 @@
 console.log('neco');
 
+/*Založte proměnné pro jméno, věk, váhu v kg a  výšku  v metrech - nastavte jim libovolnou hodnotu.
+Spočtěte BMI
+Do konzole vypište větu ve formátu:
 
+Jmenuji se Petra, je mi 34 let, vážím 65kg,  měřím 1.67m a moje BMI je 23,2.*/
+
+let jmeno = 'Marie';
+let vek = 41;
+let vaha = '56 kg';
+let vyska = '1.77 m ';
+
+BMI = vaha / (vyska * vyska);
