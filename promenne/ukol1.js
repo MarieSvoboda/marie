389515e17d82496cbbe15d893584c17803276465
MarieSvoboda <mfial@seznam.cz>
2022-02-22 18:19:48 +0100

    1 file modified

--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -8,9 +8,36 @@ Jmenuji se Petra, je mi 34 let, vážím 65kg,  měřím 1.67m a moje BMI je 23
 
 let jmeno = 'Marie';
 let vek = 41;
-let vaha = '56 kg';
-let vyska = '1.77 m ';
+let vaha = 58;
+let vyska = 1.77;
 
-BMI = vaha / (vyska * vyska);
+BMI = vaha / vyska ** 2;
+console.log(BMI);
 
-console.log('jmenuji se')
+console.log(
+  'Jmenuji se' +
+    ' ' +
+    jmeno +
+    '' +
+    ' ' +
+    'je mi' +
+    ' ' +
+    vek +
+    ' ' +
+    'vazim' +
+    ' ' +
+    vaha +
+    'kg' +
+    '.'
+);
+
+/* Nadefinujme si proměnné a jejich hodnoty potřebné k uložení následujících informací:
+ Jméno muže a ženy ​
+ Rok a měsíc narození muže i ženy​
+Spočteme rozdíl věku muže a ženy v měsících a vypíšeme ve formátu​ "[muz] a [zena] jsou od sebe věkově vzdáleni [XY] měsíců​"
+Pro zjednodušení úkolu počítejme, že je muž vždy starší! */
+
+let muzJmeno;
+let zenaJmeno;
+let rokNarozeniMuz;
+let rokNarozeni;
