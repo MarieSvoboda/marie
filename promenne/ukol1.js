f58f36bf042e7d623c0af54338257cd2af0023a3
MarieSvoboda <mfial@seznam.cz>
2022-02-24 16:29:39 +0100

    1 file modified

--- promenne/ukol1.js
+++ promenne/ukol1.js
@@ -102,5 +102,48 @@ if (a > b) {
   }
 }
 
+/*Nechte uživatele zadat 3 čísla - strany trojúhelníku a zjistěte, zda jde trojúhelník sestrojit
+- trojuhelnik jde sestrojit, pokud soucet 2 nejmensich stran je > nez treti strana
+- principialne jde POUZE o podminku navic do prikladu 2...*/
+
 //funkce
 // let nejvetsi = Math.max (a,b,c)
+let a = Number(prompt('zadej cislo A:'));
+let b = Number(prompt('zadej cislo B:'));
+let c = Number(prompt('zadej cislo C:'));
+
+if (a > b) {
+  //porovnavam jeste A a C
+  if (a > c) {
+    console.log('nejvetsi je A:' + a);
+    if (b + c > a) {
+      console.log('lze sestrojit');
+    } else {
+      console.log('nelze sestrojit');
+    }
+  } else {
+    console.log('nejvetsi je C:' + c);
+    if (b + a > c) {
+      console.log('lze sestrojit');
+    } else {
+      console.log('nelze sestrojit');
+    }
+  }
+} else {
+  //porovnavam B a C
+  if (b > c) {
+    console.log('nejvetsi je B:' + b);
+    if (a + c > b) {
+      console.log('lze sestrojit');
+    } else {
+      console.log('nelze sestrojit');
+    }
+  } else {
+    console.log('nejvetsi je C:' + c);
+    if (b + a > c) {
+      console.log('lze sestrojit');
+    } else {
+      console.log('nelze sestrojit');
+    }
+  }
+}
